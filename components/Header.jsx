6978b552cb165c5d8027532e286b6c26643cfed9
Audipunk <audipunkwp@gmail.com>
2025-02-24 17:54:33 +0200

    src/main.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,14 @@
-// Creates the header element
+import React from "react"
+
 export default function Header() {
     return (
-      <header className="header">
-        <img src="./images/troll-face.png" className="header--image" />
-        <h2 className="header--title">Meme Generator</h2>
-        <h4 className="header--project">React Course - Project 3</h4>
-      </header>
-    );
-  }
+        <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
