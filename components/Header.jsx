efccc56139879bc6716c5753d2ba9171348a3835
Audipunk <audipunkwp@gmail.com>
2025-02-27 17:43:14 +0200

    index.js

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react"
+import Header from ",/components/Header"
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
+            src="./images/troll-face.png" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">React - Project - DJS-07</h4>
         </header>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/Header";
-import Meme from "../components/Meme";
+import Header from "../components/Header"
+import Meme from "../components/Meme"
 
 export default function App() {
   return (
@@ -8,5 +8,5 @@ export default function App() {
       <Header />
       <Meme />
     </div>
-  );
+  )
 }
