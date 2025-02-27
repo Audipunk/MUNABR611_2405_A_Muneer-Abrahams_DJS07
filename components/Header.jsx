42c2a04168157b2e2cf3f4cce6d7b0712bd317c8
Audipunk <audipunkwp@gmail.com>
2025-02-28 00:44:10 +0200

    src/index.css

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <img src="./images/troll-face.png" className="header--image" />
-      <h2 className="header--title">Meme Generator</h2>
+      <h1 className="header--title">Meme Generator</h1>
       <h4 className="header--project">React Course - DJS07</h4>
     </header>
   );
