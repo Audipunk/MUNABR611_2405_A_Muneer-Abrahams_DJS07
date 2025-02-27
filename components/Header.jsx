a0a867a0d861e16ddede23ec648efba988b95af2
Audipunk <audipunkwp@gmail.com>
2025-02-27 20:16:08 +0200

    index.js

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-            src="./images/troll-face.png" 
+            src="./public\vite.svg" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
